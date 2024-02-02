51c2c5ec80333091944e7a499e1ce18709b5dd69
chenyu-jiang <jchenyu@connect.hku.hk>
2024-02-02 14:24:11 +0000

    doesn't work

--- benchmarks/weight_explorer/constrained_least_squares.py
+++ benchmarks/weight_explorer/constrained_least_squares.py
@@ -1,8 +1,10 @@
 import gurobipy as gp
 import numpy as np
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Lasso, LinearRegression
 from numpy.typing import NDArray
 import tqdm
+import glmnet
+import torch
 
 from vllm.model_executor.weight_utils import (default_weight_loader,
                                               hf_model_weights_iterator)
@@ -30,9 +32,22 @@ def solve_problem_l1_penalized(target: NDArray, candidate: NDArray, l1_lambda: f
 
 def solve_problem_sklearn_lasso(target: NDArray, candidate: NDArray, l1_lambda: float):
     # least squares loss
-    model = Lasso(alpha=l1_lambda, fit_intercept=False, selection="random", tol=1e-4, max_iter=10000)
+    model = Lasso(alpha=l1_lambda, fit_intercept=False, selection="random", tol=1e-6, max_iter=10000)
     model.fit(candidate, target)
     return model.coef_
+
+def solve_problem_sklearn_linear(target: NDArray, candidate: NDArray):
+    # least squares loss
+    model = LinearRegression(fit_intercept=False)
+    model.fit(candidate, target)
+    return model.coef_
+
+def solve_problem_glmnet(target: NDArray, candidate: NDArray):
+    glmnet_lasso = glmnet.ElasticNet(alpha=1.0, fit_intercept=False, n_jobs=32, n_splits=0)
+    glmnet_lasso.fit(candidate, target)
+    lambdas = glmnet_lasso.lambda_path_
+    coeffs = glmnet_lasso.coef_path_
+    return lambdas, coeffs
 
 def find_most_similar_column(target: NDArray, candidate: NDArray):
     best_norm = np.inf
@@ -50,49 +65,75 @@ def main():
 
     exper_id_to_params = {}
     for name, param in hf_model_weights_iterator("mistralai/Mixtral-8x7B-v0.1"):
-        if "layers.0" in name and "experts" in name and "w2" in name:
+        if "layers.0" in name and "experts" in name and "w1" in name:
             expert_id = int(name.split(".")[5])
             exper_id_to_params[expert_id] = param.float().numpy()
 
-    f = open(f"./expert_reconstruct_all_ks.csv", "w")
-    f.write("expert_id,col_id,k,n_candidate_exps,mean_diff,max_diff,norm\n")
+    f = open(f"./expert_reconstruct_all_ks_avg_experts_down_proj.csv", "w")
+    f.write("expert_id,col_id,k,n_candidate_exps,l1_lambda,mean_diff,max_diff,norm\n")
     f.flush()
 
     def _predict(c, cand, k1):
         # select top k coefficients and zero out the rest
-        ind = np.argsort(np.abs(c))[-k1:]
-        import code
-        code.interact(local=locals())
-        new_c = np.zeros_like(c)
+        ind = torch.argsort(torch.abs(c))[-k1:]
+        new_c = torch.zeros_like(c)
         new_c[ind] = c[ind]
-        return cand @ new_c
+        # ind = np.argsort(np.abs(c))[-k1:]
+        # new_c = np.zeros_like(c)
+        # new_c[ind] = c[ind]
+        return cand @ new_c.float()
 
     candidate_ks = [16, 32, 64, 128, 256, 512, 1024, 2048]
     for target_expert in tqdm.trange(8, desc="Target expert"):
         for col_id in [512]:
         # for col_id in tqdm.trange(0, 14336, 512, desc="Column", leave=False):
             target = exper_id_to_params[target_expert][:, col_id]
-            candidate = np.concatenate([exper_id_to_params[i] for i in range(8) if i != target_expert], axis=1)
+            target_cuda = torch.tensor(target).cuda()
+            # candidate = np.concatenate([exper_id_to_params[i] for i in range(8) if i != target_expert], axis=1)
+            # candidate = np.concatenate([exper_id_to_params[0], exper_id_to_params[1]], axis=1)
+            candidate = (torch.sum(torch.stack([torch.tensor(exper_id_to_params[i]).cuda() for i in range(8)]), dim=0) / 8).squeeze().cpu().numpy()
+            candidate_cuda = torch.tensor(candidate).cuda()
+            # normalizer
+            # normalizer = np.linalg.norm(target)
+            # normalizer = 0.01
+            # print("Normalizer: ", normalizer)
+            # normed_target = target / normalizer
+            # normed_candidate = candidate / normalizer
             # find the best L1 lambda
             best_norm = {k: np.inf for k in candidate_ks}
             best_mean_diff = {k: None for k in candidate_ks}
             best_max_diff = {k: None for k in candidate_ks}
-            for l1_lambda in tqdm.tqdm([100, 10, 1, 0.1, 0.01, 0.001, 1e-4], leave=False, desc="L1 lambda"):
+            best_l1_lambda = {k: None for k in candidate_ks}
+            lambdas, coeffs = solve_problem_glmnet(target, candidate)
+            for l1_lambda, coeff in zip(lambdas, coeffs.T):
+            # for l1_lambda in tqdm.tqdm([10, 5, 1, 0.1, 0.01, 0.001, 1e-4, 1e-5, 1e-6], leave=False, desc="L1 lambda"):
                 # coeffs = solve_problem_l1_penalized(target, candidate, l1_lambda)
-                coeffs = solve_problem_sklearn_lasso(target, candidate, l1_lambda)
+                # coeffs = solve_problem_sklearn_lasso(normed_target, normed_candidate, l1_lambda)
+                # coeffs = solve_problem_glmnet(target, candidate)
+                # coeffs = solve_problem_sklearn_linear(target, candidate)
+                nonzeros = np.count_nonzero(coeff)
+                tqdm.tqdm.write("L1 lambda: {}, nonzero entries in solution: {}".format(l1_lambda, nonzeros))
+                if (nonzeros == 0):
+                    continue
+                coeff_cuda = torch.tensor(coeff).cuda()
                 for k in [16, 32, 64, 128, 256, 512, 1024, 2048]:
-                    predicted = _predict(coeffs, candidate, k)
+                    # predicted = _predict(coeff, candidate, k)
+                    predicted = _predict(coeff_cuda, candidate_cuda, k)
                     # col_id = find_most_similar_column(target, candidate)
                     # predicted = candidate[:, col_id]
-                    mean_diff = np.mean(np.abs(target - predicted))
-                    max_diff = np.max(np.abs(target - predicted))
-                    norm = np.linalg.norm(target - predicted)
+                    # mean_diff = np.mean(np.abs(target - predicted))
+                    # max_diff = np.max(np.abs(target - predicted))
+                    # norm = np.linalg.norm(target - predicted)
+                    mean_diff = torch.mean(torch.abs(target_cuda - predicted)).item()
+                    max_diff = torch.max(torch.abs(target_cuda - predicted)).item()
+                    norm = torch.norm(target_cuda - predicted).item()
                     if norm < best_norm[k]:
                         best_norm[k] = norm
                         best_mean_diff[k] = mean_diff
                         best_max_diff[k] = max_diff
+                        best_l1_lambda[k] = l1_lambda
             for k in candidate_ks:
-                f.write(f"{target_expert},{col_id},{k},7,{best_mean_diff[k]},{best_max_diff[k]},{best_norm[k]}\n")
+                f.write(f"{target_expert},{col_id},{k},7,{best_l1_lambda[k]},{best_mean_diff[k]},{best_max_diff[k]},{best_norm[k]}\n")
                 f.flush()
     f.close()
 
